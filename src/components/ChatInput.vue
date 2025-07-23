e6726c854b3668b9d27f7f4901a91fa31db35f0a
Nick Berens <nickberens360@gmail.com>
2025-07-23 08:51:48 -0500

    fix maxLengthThreshold mismatch

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -133,7 +133,7 @@ export default {
   setup(props, { emit }) {
     const textareaRef = ref(null);
     const maxLength = 1000; // Match backend Query model limit
-    const warningThreshold = 1800; // 90% of max length
+    const warningThreshold = 900; // 90% of max length
 
     // Character counting and warning states
     const characterCount = computed(() => props.userInput.length);
