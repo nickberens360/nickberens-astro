c5eca8860be29f2cf93cffc7531536af4e2a1490
Nick Berens <nickberens360@gmail.com>
2025-07-26 07:44:11 -0500

    fix potential empty bot items

--- src/components/ChatMessageList.vue
+++ src/components/ChatMessageList.vue
@@ -29,7 +29,7 @@
               <div class="bot-message-wrapper">
 
                   <div
-                    v-if="messages[index + 1].text"
+                    v-if="messages[index + 1] && messages[index + 1].text"
                     class="markdown-content-wrapper"
                   >
                     <span
